ce242bb68c0cb3f564dab3cee4f9542a548f82e9
Pawel Badenski <Pawel.Badenski@thoughtworks.com>
2012-08-13 10:10:15 +0100

    Schedule all nongovuk collectors
    @author Seema
    @author Pawel

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,6 @@
 every :sunday, :at => '1am' do
-  command "cd #{File.expand_path(File.dirname(__FILE__) + "/../")} && bundle exec bin/collector broadcast"
+  command "cd #{File.expand_path(File.dirname(__FILE__) + "/../")} && RACK_ENV=production bundle exec bin/collector -s=business_link -m=visits broadcast"
+  command "cd #{File.expand_path(File.dirname(__FILE__) + "/../")} && RACK_ENV=production bundle exec bin/collector -s=directgov -m=visits broadcast"
+  command "cd #{File.expand_path(File.dirname(__FILE__) + "/../")} && RACK_ENV=production bundle exec bin/collector -s=business_link -m=visitors broadcast"
+  command "cd #{File.expand_path(File.dirname(__FILE__) + "/../")} && RACK_ENV=production bundle exec bin/collector -s=directgov -m=visitors broadcast"
 end
