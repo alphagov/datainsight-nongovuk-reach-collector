c9627be881cf066db840414e22b79d26ccb32b57
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-08-17 10:20:39 +0100

    Update scheduling
    Run every Monday and Tuesday at 4pm
    
    author: @robyoung
    author: @cpawar

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-every :sunday, :at => '1am' do
+every [:monday, :tuesday], :at => '4pm' do
   command "cd #{File.expand_path(File.dirname(__FILE__) + "/../")} && RACK_ENV=production bundle exec bin/collector -s=businesslink -m=visits broadcast"
   command "cd #{File.expand_path(File.dirname(__FILE__) + "/../")} && RACK_ENV=production bundle exec bin/collector -s=directgov -m=visits broadcast"
   command "cd #{File.expand_path(File.dirname(__FILE__) + "/../")} && RACK_ENV=production bundle exec bin/collector -s=businesslink -m=visitors broadcast"
