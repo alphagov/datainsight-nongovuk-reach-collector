8635f30f7005422eb7d7ee27628174dbd1cd3104
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-08-23 10:28:11 +0100

    Factor out google credentials and auth token.
    credentials file: /etc/gds/google_credentials.yml
    auth token file:  /var/lib/gds/google-drive-token.yml
    
    author: @jthoenes
    author: @robyoung

--- lib/nongovuk_reach_collector.rb
+++ lib/nongovuk_reach_collector.rb
@@ -4,18 +4,17 @@ require "json"
 module Collectors
   class NongovukReachCollector
 
-    API_SCOPE = "https://docs.google.com/feeds/ https://docs.googleusercontent.com/ https://spreadsheets.google.com/feeds/"
+    GOOGLE_API_SCOPE = "https://docs.google.com/feeds/ https://docs.googleusercontent.com/ https://spreadsheets.google.com/feeds/"
+    GOOGLE_CREDENTIALS = '/etc/gds/google_credentials.yml'
+    GOOGLE_TOKEN = "/var/lib/gds/google-drive-token.yml"
+
     SPREADSHEET_KEY="0Aq4mEanyr9A2dDVyMEpaNV9DbXZuNDV6cnJ5QzVyVVE"
 
-    GOOGLE_CLIENT_ID = '1054017153726.apps.googleusercontent.com'
-    GOOGLE_CLIENT_SECRET = 'eMFsc8LU3ZGrRFG93WfQCnD3'
-
-    Infinity      = 1.0/0
     TYPE_TO_ROW = {
-        "businesslink:visits" => 37,
-        "directgov:visits" => 38,
-        "businesslink:visitors" => 31,
-        "directgov:visitors" => 32
+      "businesslink:visits" => 37,
+      "directgov:visits" => 38,
+      "businesslink:visitors" => 31,
+      "directgov:visitors" => 32
     }
 
     def initialize(site, metric, auth_code = nil)
@@ -57,58 +56,59 @@ module Collectors
       row = TYPE_TO_ROW[type] or raise "Unkown type: `#{type}`"
 
       messages = []
-      (2..Infinity).each do |col|
+      col = 2
+      loop do
         break if get_date(worksheet, col) >= Date.today
         value = (worksheet[row, col] == "" || worksheet[row, col].nil?) ? nil : worksheet[row, col].gsub(",", "").to_i
         messages <<
-            create_message(
-                value,
-                get_date(worksheet, col).strftime,
-                (get_date(worksheet, col) + 7).strftime)
+          create_message(
+            value,
+            get_date(worksheet, col).strftime,
+            (get_date(worksheet, col) + 7).strftime)
+
+        col += 1
       end
       messages
     end
 
     private
 
-    #ToDO what if spreadsheet is not existing
     def get_worksheet
-      session = authenticate
-      session.spreadsheet_by_key(SPREADSHEET_KEY).worksheets[0]
+      authenticate.spreadsheet_by_key(SPREADSHEET_KEY).worksheets[0]
     end
 
-    def authenticate
-      auth = GoogleAuthenticationBridge::GoogleAuthentication.new(API_SCOPE, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET)
-      token = auth.get_oauth2_access_token(@auth_code)
-      session = GoogleDrive.login_with_oauth(token)
 
-      session
+    def authenticate
+      auth = GoogleAuthenticationBridge::GoogleAuthentication.create_from_config_file(
+        GOOGLE_API_SCOPE, GOOGLE_CREDENTIALS, GOOGLE_TOKEN
+      )
+      GoogleDrive.login_with_oauth(auth.get_oauth2_access_token(@auth_code))
     end
 
     def create_message(value, start_at, end_at)
       {
-          :envelope => {
-              :collected_at => DateTime.now,
-              :collector => "nongovuk_reach",
-          },
-          :payload => {
-              :value => value,
-              :site => @site,
-              :start_at => start_at,
-              :end_at => end_at
-          }
+        :envelope => {
+          :collected_at => DateTime.now,
+          :collector => "nongovuk_reach",
+        },
+        :payload => {
+          :value => value,
+          :site => @site,
+          :start_at => start_at,
+          :end_at => end_at
+        }
       }
     end
 
     def create_exception_message(message)
       {
-          :envelope => {
-              :collected_at => DateTime.now,
-              :collector => "nongovuk_reach",
-          },
-          :payload => {
-              :error => message
-          }
+        :envelope => {
+          :collected_at => DateTime.now,
+          :collector => "nongovuk_reach",
+        },
+        :payload => {
+          :error => message
+        }
       }
     end
 
