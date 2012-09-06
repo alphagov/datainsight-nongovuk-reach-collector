6b2b8be58614eef70c5e354dba44aaace3387e44
Johannes Thoenes <jthoenes@thoughtworks.com>
2012-09-06 17:32:50 +0100

    Adding out and error log for cronjobs.
    author: @jthoenes

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,14 @@
+def cron_command(site, metric)
+  root_path = File.expand_path(File.dirname(__FILE__) + "/../")
+
+  "cd #{root_path} && RACK_ENV=production bundle exec bin/collector -s=#{site} -m=#{metric} broadcast" +
+  " >> #{root_path}/log/cron.out.log 2>> #{root_path}/log/cron.err.log"
+end
+
+
 every [:monday, :tuesday], :at => '4pm' do
-  command "cd #{File.expand_path(File.dirname(__FILE__) + "/../")} && RACK_ENV=production bundle exec bin/collector -s=businesslink -m=visits broadcast"
-  command "cd #{File.expand_path(File.dirname(__FILE__) + "/../")} && RACK_ENV=production bundle exec bin/collector -s=directgov -m=visits broadcast"
-  command "cd #{File.expand_path(File.dirname(__FILE__) + "/../")} && RACK_ENV=production bundle exec bin/collector -s=businesslink -m=visitors broadcast"
-  command "cd #{File.expand_path(File.dirname(__FILE__) + "/../")} && RACK_ENV=production bundle exec bin/collector -s=directgov -m=visitors broadcast"
+  command cron_command(site = "businesslink", metric = "visits")
+  command cron_command(site = "directgov", metric = "visits")
+  command cron_command(site = "businesslink", metric = "visitors")
+  command cron_command(site = "directgov", metric = "visitors")
 end
